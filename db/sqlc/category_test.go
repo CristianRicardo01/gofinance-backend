124079999c07875e132174cdabc01fae92b57d2b
Cristian Ricardo <cristiansilv50@gmail.com>
2022-11-05 16:31:29 -0400

    test: cange category of project

--- db/sqlc/category_test.go
+++ db/sqlc/category_test.go
@@ -19,7 +19,7 @@ func createRandomCategory(t *testing.T) Category {
 
 	category, err := testQueries.CreateCategory(context.Background(), arg)
 	require.NoError(t, err)
-	require.NotEmpty(t, user)
+	require.NotEmpty(t, category)
 
 	require.Equal(t, arg.UserID, category.UserID)
 	require.Equal(t, arg.Title, category.Title)
@@ -49,24 +49,24 @@ func TestGetCategory(t *testing.T) {
 
 func TestDeleteCategory(t *testing.T) {
 	category := createRandomCategory(t)
-	err := testQueries.DeleteCategory(context.Background(), category.ID)
+	err := testQueries.DeleteCategories(context.Background(), category.ID)
 	require.NoError(t, err)
 }
 
-func TestUpdataCategory(t *testing.T) {
+func TestUpdateCategory(t *testing.T) {
 	category1 := createRandomCategory(t)
 
-	arg := UpdateCategoryParams{
+	arg := UpdateCategoriesParams{
 		ID:          category1.ID,
 		Title:       util.RandomString(12),
 		Description: util.RandomString(20),
 	}
 
-	category2, err := testQueries.UpdateCategory(context.Background(), arg)
+	category2, err := testQueries.UpdateCategories(context.Background(), arg)
 	require.NoError(t, err)
 	require.NotEmpty(t, category2)
 
-	require.Equal(t, category1.UserID, category2.UserID)
+	require.Equal(t, category1.ID, category2.ID)
 	require.Equal(t, arg.Title, category2.Title)
 	require.Equal(t, arg.Description, category2.Description)
 	require.NotEmpty(t, category2.CreatedAt)
@@ -86,7 +86,7 @@ func TestListCategories(t *testing.T) {
 	require.NoError(t, err)
 	require.NotEmpty(t, categorys)
 
-	for _,category := range categorys {
+	for _, category := range categorys {
 		require.Equal(t, lastCategory.ID, category.ID)
 		require.Equal(t, lastCategory.UserID, category.UserID)
 		require.Equal(t, lastCategory.Title, category.Title)
