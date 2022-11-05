215b578310dce25bb26a6681f28c59b8d8d119cc
Cristian Ricardo <cristiansilv50@gmail.com>
2022-11-05 16:21:42 -0400

    feat: add account query to crud

--- db/query/account.sql
+++ db/query/account.sql
@@ -1,14 +1,14 @@
 -- name: CreateAccount :one
 INSERT INTO accounts (
-    user_id,
-    category_id,
-    title,
-    type,
-    description,
-    value,
-    date
+  user_id,
+  category_id,
+  title,
+  type,
+  description,
+  value,
+  date
 ) VALUES (
-    $1, $2, $3, $4, $5, $6, $7
+  $1, $2, $3, $4, $5, $6, $7
 ) RETURNING *;
 
 -- name: GetAccount :one
@@ -16,36 +16,47 @@ SELECT * FROM accounts
 WHERE id = $1 LIMIT 1;
 
 -- name: GetAccounts :many
-SELECT 
-    a.id,
-    a.user_id,
-    a.title,
-    a.type,
-    a.description,
-    a.value,
-    a.date,
-    a.created_at,
-    c.title as category_title
-FROM accounts a
-LEFT JOIN categories c ON c.id = a.category_id
-WHERE a.user_id = $1 AND a.type = $2
-AND a.category_id = $3 AND a.title LIKE $4
-AND a.description LIKE $5 AND a.date = $6;
+SELECT
+  a.id,
+  a.user_id,
+  a.title,
+  a.type,
+  a.description,
+  a.value,
+  a.date,
+  a.created_at,
+  c.title as category_title
+FROM
+  accounts a
+LEFT JOIN
+  categories c ON c.id = a.category_id
+WHERE
+  a.user_id = @user_id
+AND
+  a.type = @type
+AND
+  LOWER(a.title) LIKE CONCAT('%', LOWER(@title::text), '%')
+AND
+  LOWER(a.description) LIKE CONCAT('%', LOWER(@description::text), '%')
+AND
+  a.category_id = COALESCE(sqlc.narg('category_id'), a.category_id)
+AND
+  a.date = COALESCE(sqlc.narg('date'), a.date);
 
--- name: GetAccontReports :one
-SELECT SUM (value) AS sum_value FROM accounts
-WHERE user_id = $1 AND type = $2;
+-- name: GetAccountsReports :one
+SELECT SUM(value) AS sum_value FROM accounts
+where user_id = $1 and type = $2;
 
--- name: GetAccontGraph :one
-SELECT COUNT (*) FROM accounts
-WHERE user_id = $1 AND type = $2;
+-- name: GetAccountsGraph :one
+SELECT COUNT(*) FROM accounts
+where user_id = $1 and type = $2;
 
--- name: UpdateAccont :one
+-- name: UpdateAccount :one
 UPDATE accounts
 SET title = $2, description = $3, value = $4
-WHERE id = $1 
+WHERE id = $1
 RETURNING *;
 
--- name: DeleteCAccout :exec
+-- name: DeleteAccount :exec
 DELETE FROM accounts
 WHERE id = $1;
