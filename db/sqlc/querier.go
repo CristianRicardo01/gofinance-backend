214b62c360d0a5bba0147a51a3a9d312585919fb
Cristian Ricardo <cristiansilv50@gmail.com>
2022-11-05 16:35:22 -0400

    feat: add querier and store

--- db/sqlc/querier.go
+++ db/sqlc/querier.go
@@ -12,18 +12,18 @@ type Querier interface {
 	CreateAccount(ctx context.Context, arg CreateAccountParams) (Account, error)
 	CreateCategory(ctx context.Context, arg CreateCategoryParams) (Category, error)
 	CreateUser(ctx context.Context, arg CreateUserParams) (User, error)
-	DeleteAccout(ctx context.Context, id int32) error
-	DeleteCategory(ctx context.Context, id int32) error
-	GetAccontGraph(ctx context.Context, arg GetAccontGraphParams) (int64, error)
-	GetAccontReports(ctx context.Context, arg GetAccontReportsParams) (int64, error)
+	DeleteAccount(ctx context.Context, id int32) error
+	DeleteCategories(ctx context.Context, id int32) error
 	GetAccount(ctx context.Context, id int32) (Account, error)
 	GetAccounts(ctx context.Context, arg GetAccountsParams) ([]GetAccountsRow, error)
+	GetAccountsGraph(ctx context.Context, arg GetAccountsGraphParams) (int64, error)
+	GetAccountsReports(ctx context.Context, arg GetAccountsReportsParams) (int64, error)
 	GetCategories(ctx context.Context, arg GetCategoriesParams) ([]Category, error)
 	GetCategory(ctx context.Context, id int32) (Category, error)
 	GetUser(ctx context.Context, username string) (User, error)
 	GetUserById(ctx context.Context, id int32) (User, error)
-	UpdateAccont(ctx context.Context, arg UpdateAccontParams) (Account, error)
-	UpdateCategory(ctx context.Context, arg UpdateCategoryParams) (Category, error)
+	UpdateAccount(ctx context.Context, arg UpdateAccountParams) (Account, error)
+	UpdateCategories(ctx context.Context, arg UpdateCategoriesParams) (Category, error)
 }
 
 var _ Querier = (*Queries)(nil)
