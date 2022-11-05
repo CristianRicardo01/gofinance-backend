76e3893c955003719ae50185f5ef3fc62f020f35
Cristian Ricardo <cristiansilv50@gmail.com>
2022-11-05 15:39:19 -0400

    feat: add main to start server

--- main.go
+++ main.go
@@ -7,17 +7,16 @@ import (
 	"github.com/GustavoNoronha0/gofinance-backend/api"
 	db "github.com/GustavoNoronha0/gofinance-backend/db/sqlc"
 	_ "github.com/lib/pq"
-	"golang.org/x/tools/cmd/getgo/server"
 )
 
 const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://postgres:postgres@localhost:5432/go_finance?sslmode=disable"
 	serverAddress = "0.0.0.0:8000"
 )
 
 func main() {
-	conn, err := sql.Open(dbDriver,)
+	conn, err := sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -26,7 +25,7 @@ func main() {
 	server := api.NewServer(store)
 
 	err = server.Start(serverAddress)
-	if err !=nil{
+	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
 }
